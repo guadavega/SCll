39e297a53632910e7e7d0869fe6be494b6b5757b
Guadalupe Vega <guadavega.me@gmail.com>
2024-04-17 22:36:52 -0300

    Actividad 1 Actualizada

--- TP1/TP1_6.m
+++ TP1/TP1_6.m
@@ -1,67 +1,40 @@
-clc, clear all, close all;
-%Item 6
+clear all;close all;
 
-%Se implementa un PID en tiempo discreto para que el ángulo del motor 
-%permanezca en una referencia de 1 radian
-
-color_='b';
-X = -[0; 0];                   %definimos condiciones iniciales
+X = -[0; 0; 0];
 ii = 0;
-tS = 1e-7;
-tF = 0.01; 
+t_etapa = 1e-7;
 wRef = 2;
- 
-                                    %constantes del PID
-Kp=0.1;
-Ki=0.01;
-Kd=5;                             
-                          
-A1=((2*Kp*tS)+(Ki*(tS^2))+(2*Kd))/(2*tS);
-B1=(-2*Kp*tS+Ki*(tS^2)-4*Kd)/(2*tS);
-C1=Kd/tS;
-e=zeros(tF/tS,1);
-u=0;
+tF = 1e-3;
 
-                                      %parámetros del motor
-Laa = 366e-6;
-J = 5e-9;
-Ra = 55.6;
-B = 0;
-Ki = 6.49e-3;
-Km = 6.53e-3;
+Kp = 0.1;
+Ki = 0.01;
+%Kd = 5;
+Kd = 2.1;       %se modifica para evitar el sobrepaso en la salida del sistema
+color_='b';
+Ts = t_etapa;
+A1 = ((2*Kp*Ts)+(Ki*(Ts^2))+(2*Kd))/(2*Ts);
+B1 = (-2*Kp*Ts+Ki*(Ts^2)-4*Kd)/(2*Ts);
+C1 = Kd/Ts;
+e = zeros(tF/t_etapa,1);
+u = 0;
 
-%Predefinir vectores para almacenar resultados
-x1 = zeros(1, length(0:tS:tF));         %Vector para almacenar la salida del sistema
-acc = zeros(1, length(0:tS:tF));        %Vector para almacenar la entrada del sistema
-
-for t=0:tS:tF
-    ii=ii+1;k=ii+2;
-    Va=u;                                    %tensión de entrada al motor
+for t=0:t_etapa:tF
     
-    h=1e-7;
-    w= X(1);
-    wp= X(2);
+    ii=ii+1;
+    k=ii+2;
+    X=modmotor(t_etapa, X, u);
+    e(k)=wRef-X(3); %ERROR
+    u=u+A1*e(k)+B1*e(k-1)+C1*e(k-2); %PID
     
-    for ii=1:(tS/h)
-        wpp =(-wp*(Ra*J+Laa*B)-w*(Ra*B+Ki*Km)+Va*Ki)/(J*Laa);
-        wp = wp+h*wpp;
-        w = w + h*wp;
-    end
+    x1(ii)=X(1);%Omega
+    x2(ii)=X(2);%wp
+    theta(ii)=X(3);%tita
     
-    X = [w; wp];                            %actualización del estado del motor
-    x1(ii)=X(1);                            %Omega (w)
-    x2(ii)=X(2);                            %wp
-
-    e(k)=wRef-X(1);                         %error
-    u=u+A1*e(k)+B1*e(k-1)+C1*e(k-2);        %ecuación del PID
     acc(ii)=u;
 end
-
-t=0:tS:tF;
-subplot(2,1,1);
+t=0:t_etapa:tF;
+subplot(2,1,1);hold on;
 plot(t,x1,color_);title('Salida y, \omega_t');
-subplot(2,1,2);
-hold on;
-plot(t,acc,color_);title('Entrada u_t, v_a');
+subplot(2,1,2);hold on;
+plot(t,theta,color_); title('Salida Controlada por PID');
 xlabel('Tiempo [Seg.]');
-
---
+++ TP1/modmotor.m
@@ -0,0 +1,20 @@
+function [X]=modmotor(t_etapa, xant, u)
+Laa=366e-6; 
+J=5e-9;
+Ra=55.6;
+B=0;
+Ki=6.49e-3;
+Km=6.53e-3;
+Va=u;
+h=1e-7;
+omega= xant(1);
+wp = xant(2);
+theta = xant(3);
+for ii=1:t_etapa/h
+wpp =(-wp*(Ra*J+Laa*B)-omega*(Ra*B+Ki*Km)+Va*Ki)/(J*Laa);
+wp = wp+h*wpp;
+omega = omega + h*wp;
+theta_p = omega;
+theta = theta + h*theta_p;
+end
+X=[omega,wp,theta];
